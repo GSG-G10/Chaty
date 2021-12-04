2a077a0b9d9c64fbc5fd3e53736b367a22955e49
NourAdawi <nouradwi12@gmail.com>
2021-12-04 14:32:08 +0200

    edit responsive

--- client/src/containers/chatContainer.js
+++ client/src/containers/chatContainer.js
@@ -1,3 +1,6 @@
 import { Box } from '@mui/system';
+import Image from '../assets/bg.jpg';
 
-export const Chatcontainer = () => <Box>Hello!</Box>;
+export const Chatcontainer = () => (
+  <Box height="100vh" width="100%" sx={{ backgroundImage: `url(${Image})` }} />
+);
