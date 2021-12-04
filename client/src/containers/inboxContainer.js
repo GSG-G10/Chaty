8d312e34e61238db7505aa1ab30fb23752e3d098
NourAdawi <nouradwi12@gmail.com>
2021-12-04 15:21:44 +0200

    edit size

--- client/src/containers/inboxContainer.js
+++ client/src/containers/inboxContainer.js
@@ -4,7 +4,7 @@ import { HeaderTabs } from '../components/headerTabs';
 import { SearchBar } from '../components/searchBar';
 
 export const InboxContainer = () => (
-  <Box width="45%" minWidth="350px" bgcolor="#ededed" height="50vh">
+  <Box width="40%" minWidth="350px" bgcolor="#ededed" height="50vh">
     <Box Height="16vh">
       <SearchBar />
       <HeaderTabs />
